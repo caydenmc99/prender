52f023fe90fcf2781ecf2b257191ca26198df0e7
Mike Powers <mikepowersofficial@gmail.com>
2024-06-02 15:58:25 -0400

    Run

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-const express = require("express");
-const { scrapeLogic } = require("./scrapeLogic");
-const app = express();
+const puppeteer = require("puppeteer");
+require("dotenv").config();
 
-const PORT = process.env.PORT || 4000;
+const scrapeLogic = async () => {
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
 
-app.get("/scrape", (req, res) => {
-  scrapeLogic(res);
-});
+  const page = await browser.newPage();
 
-app.get("/", (req, res) => {
-  res.send("Render Puppeteer server is up and running!");
-});
+  // Function to perform the scraping
+  const scrape = async () => {
+    try {
+      await page.goto("https://hrznlabs.com/");
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+      // Extract all the text from the page
+      const pageContent = await page.evaluate(() => document.body.innerText);
+
+      // Print the extracted text
+      console.log(pageContent);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  // Run the scraping function every 10 seconds
+  setInterval(scrape, 10000);
+};
+
+// Run the scrapeLogic function when the script is executed
+scrapeLogic();
