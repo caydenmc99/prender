16fa48ddf3374411db062326e41680b70b4c06d5
Mike Powers <mikepowersofficial@gmail.com>
2024-06-02 16:18:56 -0400

    yolo

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -14,34 +14,20 @@ const scrapeLogic = async (res) => {
         ? process.env.PUPPETEER_EXECUTABLE_PATH
         : puppeteer.executablePath(),
   });
+
   try {
     const page = await browser.newPage();
 
-    await page.goto("https://developer.chrome.com/");
+    await page.goto("https://www.aiamastery.com/", { timeout: 30000 }); // Increase the timeout to 60 seconds
 
-    // Set screen size
-    await page.setViewport({ width: 1080, height: 1024 });
+    // Extract all the text from the page
+    const pageContent = await page.evaluate(() => document.body.innerText);
 
-    // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
-
-    // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
-
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
-
-    // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`;
-    console.log(logStatement);
-    res.send(logStatement);
+    // Print the extracted text
+    console.log(pageContent);
+    res.send(pageContent);
   } catch (e) {
-    console.error(e);
+    console.error("Error during scraping:", e);
     res.send(`Something went wrong while running Puppeteer: ${e}`);
   } finally {
     await browser.close();
